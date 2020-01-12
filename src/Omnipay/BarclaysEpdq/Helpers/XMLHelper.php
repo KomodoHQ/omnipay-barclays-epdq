e191ce6cb2d99d5b4e515f8720fedd4c6afa1ea0
Scott Salisbury <scottsalisbury@gmail.com>
2020-01-12 23:51:43 +0000

    Added xmlToArray helper to easily convert the DirectLink SimpleXMLElement response into an array

---
+++ src/Omnipay/BarclaysEpdq/Helpers/XMLHelper.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Omnipay\BarclaysEpdq\Helpers;
+
+class XMLHelper {
+
+    /**
+     * Convert SimpleXMLElement data to array
+     *
+     * @param \SimpleXMLElement $xml
+     * @return array
+     */
+    public static function xmlToArray(\SimpleXMLElement $xml): array
+    {
+        $parser = function (\SimpleXMLElement $xml, array $collection = []) use (&$parser) {
+            $nodes = $xml->children();
+            $attributes = $xml->attributes();
+
+            if (0 !== count($attributes)) {
+                foreach ($attributes as $attrName => $attrValue) {
+                    $collection['attributes'][$attrName] = strval($attrValue);
+                }
+            }
+
+            if (0 === $nodes->count()) {
+                $collection['value'] = strval($xml);
+                return $collection;
+            }
+
+            foreach ($nodes as $nodeName => $nodeValue) {
+                if (count($nodeValue->xpath('../' . $nodeName)) < 2) {
+                    $collection[$nodeName] = $parser($nodeValue);
+                    continue;
+                }
+
+                $collection[$nodeName][] = $parser($nodeValue);
+            }
+
+            return $collection;
+        };
+
+        return [
+            $xml->getName() => $parser($xml)
+        ];
+    }
+
+}
